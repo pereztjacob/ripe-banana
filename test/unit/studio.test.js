028d45344f73e82949866b0842d16501b51c195d
Jack Lomax <lomax15@me.com>
2018-04-19 16:55:11 -0700

    functional stuido name required test

--- test/unit/studio.test.js
+++ test/unit/studio.test.js
@@ -15,4 +15,15 @@ describe('Studio model', () => {
         data._id = studio._id;
         assert.deepEqual(studio.toJSON(), data);
     });
+
+    const getValidationErrors = validation => {
+        assert.isDefined(validation, 'expected errors but got none');
+        return validation.errors;
+    };
+
+    it('name is required field', () => {
+        const studio = new Studio({});
+        const errors = getValidationErrors(studio.validateSync());
+        assert.equal(errors.name.kind, 'required');
+    });
 });
