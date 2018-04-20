0278c432b0340e6591c84eca3e59b387715efdfa
Charlie <heinercj@gmail.com>
2018-04-19 23:35:21 -0700

    review model and testing

---
+++ test/unit/reviewer.test.js
@@ -0,0 +1,22 @@
+const { assert } = require('chai');
+const Reviewer = require('../../lib/models/Reviewer');
+
+describe('reviewer model', () => {
+    it('reviewer is a valid model', () => {
+        const data = {
+            name: 'Roger Ebert',
+            company: 'roger-ebert.com'
+        };
+
+        const reviewer = new Reviewer(data);
+        data._id = reviewer._id;
+        assert.deepEqual(reviewer.toJSON(), data);
+    });
+
+    it('name and company are required', () => {
+        const reviewer = new Reviewer({});
+        const { errors } = reviewer.validateSync();
+        assert.equal(errors.name.kind, 'required');
+        assert.equal(errors.company.kind, 'required');
+    });
+});
